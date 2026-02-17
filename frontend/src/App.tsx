f25e82fb8c740609c758293398b96a851319a0c3
waamir104 <waamirdev@gmail.com>
2026-02-17 09:36:11 -0600

    Adding main app page and route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { Login } from "./pages/Login";
+import { AppPage } from "./pages/AppPage";
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/app" element={<AppPage />} />
       <Route path="*" element={<Login />} />
     </Routes>
   );
---
+++ frontend/src/pages/AppPage.tsx
@@ -0,0 +1,9 @@
+export const AppPage = () => {
+  return (
+    <section>
+      <h1>App</h1>
+      <p>Aquí iremos agregando los componentes principales de la aplicación.</p>
+    </section>
+  );
+};
+
